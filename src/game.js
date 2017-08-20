e64452d359ec0e4a378c89ea0d5ef5e4babecafb
Darko Sabranovic <dsabranovic@yahoo.com>
2017-08-21 00:25:59 +0200

    refactored some code, made cloud class, removed starting on cloud, added starting game loop of clouds

--- src/game.js
+++ src/game.js
@@ -6,99 +6,96 @@ var height = canvas.height
 
 var timer = 60
 var score = 0
-var limit = 325
+var limit = 450
 var jumpingStatus = false
 var goingDown = false
 var jumping
 var falling
+var fallingStatus = false
 var onCloud = false
 var clouds = []
-
-var hero = {
-    speed: 250,
-    x: 20,   
-    y: 590,
-    defaultX: 20,
-    defaultY: 460,
-    width: 90,
-    height: 110
+var randomX = function() {
+    return Math.random() * (width - 235)
 }
 
-var startCloud = {
-    x: 20,
+var player = {
+    speed: 275,
+    x: 500,   
     y: 700,
-    width: 235,
-    height: 56
-}
-var cloud = {
-    x: Math.random() * (width - 235),
-    y: width / 2,
-    width: 235,
-    height: 56
+    width: 80,
+    height: 100,
+    dir: ""
 }
 
-var secondCloud = {
-    x:Math.random() * (width - 235),
-    y: width / 4,
-    width: 235,
-    height: 56
+var Cloud = function(x,y,width,height,dir) {
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.dir = dir
 }
 
-var jump_y = hero.y
+var cloudOne = new Cloud(20,600,235,56, "right")
+var cloud = new Cloud(randomX(), 200, 235, 56, "left")
+var secondCloud = new Cloud(randomX(), 400, 235, 56, "right")
+
+
+clouds.push.apply(clouds, [cloudOne,secondCloud,cloud])
+var jump_y = player.y
 
 
 function moveCloud(cloud) {
-    var move = setInterval(function() {
-        if(cloud.x < 150) {
-            clearInterval(move)
-            moveCloudRight(cloud)
-        }
+    if(cloud.dir === "right") {
+        cloud.x+= 2
+    }
+    if(cloud.x > 650) {
+        cloud.dir = "left"
+    }
+    if(cloud.dir === "left") {
         cloud.x-= 2
-        
-    }, 15)
+    }
+    if(cloud.x < 150) {
+        cloud.dir  = "right"
+    }
 }
 
-var moveCloudRight = function(cloud) {
-    var move = setInterval(function() {
-        if(cloud.x > 650) {
-            clearInterval(move)
-            moveCloud(cloud)
-        }
-        cloud.x += 2
-    }, 15)
-}
 
 function jump() {
-    console.log(hero.x)
-    console.log(cloud.x)
-    if(hero.y > limit && !goingDown){
+    if(player.y > limit && !goingDown){
         jumpingStatus = true
-        hero.y-=7 ;
+        player.y-=6 ;
     } else {
-    goingDown = true;
-        hero.y +=7;
-        if(hero.y === jump_y) {
+        if(!fallingStatus) {
+            goingDown = true;
+            player.y +=6;
+        }
+
+        if(player.y === jump_y) {
             goingDown = false
             jumpingStatus = false
             clearInterval(jumping);
         }
-        console.log(hero.x)
-            if((hero.x + 15 < cloud.x + cloud.width) && (hero.x + hero.width - 15  > cloud.x) && (hero.y + hero.height > cloud.y) && (hero.y + hero.height < cloud.y + cloud.height) )  {
+           clouds.forEach(function(cloud) {
+            if((player.x + 15 < cloud.x + cloud.width) && (player.x + player.width - 15  > cloud.x) && (player.y + player.height > cloud.y ) && (player.y + player.height < cloud.y + cloud.height) )  {
+                score++
                 goingDown = false;
-                limit= 100
                 jumpingStatus = false
-                clearInterval(jumping)
                 onCloud = true
-                console.log(limit)
-            }
-            if((hero.x + 15 < secondCloud.x + secondCloud.width) && (hero.x + hero.width - 15  > secondCloud.x) && (hero.y + hero.height > secondCloud.y) && (hero.y + hero.height < secondCloud.y + secondCloud.height) )  {
-                limit = 0
-                goingDown = false
-                jumpingStatus = false
                 clearInterval(jumping)
+                console.log(player.y)
+                if(player.y < 550 && player.y > 400) {
+                    limit=350
+                }
+                if(player.y < 400 && player.y > 300) {
+                    limit= 250
+                }
+                if(player.y < 300 && player.y > 0) {
+                    limit = 150
+                }
+                
             }
-
-           
+       
+           }) 
     }
 }
 
@@ -111,22 +108,25 @@ var timerTick = setInterval(function() {
 }, 1000)
 
 var bgImage = new Image();
-var heroImage = new Image();
+var playerImage = new Image();
 var timerImage = new Image()
 var scoreImage = new Image()
 var startCloudImage = new Image()
 var randomCloud = new Image()
-var randomCloudSecond = new Image()
+var randomCloudOne = new Image()
 
 var cloudImages = ["../PNG/cloud1.png", "../PNG/cloud2.png", "../PNG/cloud3.png"]
+var getRandomCloud = function() {
+    return cloudImages[Math.floor(Math.random() * cloudImages.length)]
+}
 
 bgImage.src = "../PNG/background.png";
-heroImage.src = "../PNG/JumpJumpBoy.png";
+playerImage.src = "../PNG/JumpJumpBoy.png";
 timerImage.src = "../PNG/clock.png"
 scoreImage.src = "../PNG/cloud_counter.png"
-startCloudImage.src = cloudImages[0] 
-randomCloud.src = cloudImages[Math.floor(Math.random() * cloudImages.length)]
-randomCloudSecond.src = cloudImages[Math.floor(Math.random() * cloudImages.length)]
+startCloudImage.src = getRandomCloud()
+randomCloud.src = getRandomCloud()
+randomCloudOne.src = getRandomCloud()
 
 
 // Handle keyboard controls
@@ -143,12 +143,11 @@ addEventListener("keyup", function (e) {
 
 var render = function () {
 	ctx.drawImage(bgImage, 0, 0);
-    ctx.drawImage(heroImage, hero.x, hero.y, hero.width, hero.height);
-    ctx.drawImage(startCloudImage, startCloud.x, startCloud.y, startCloud.width, startCloud.height)
-    ctx.drawImage(randomCloud, cloud.x, cloud.y, cloud.width, cloud.height)
-    ctx.drawImage(randomCloudSecond, secondCloud.x, secondCloud.y, 235, 56 )
-
-    // Score
+    ctx.drawImage(playerImage, player.x, player.y, player.width, player.height);
+    ctx.drawImage(startCloudImage, clouds[0].x, clouds[0].y, clouds[0].width, clouds[0].height)
+    ctx.drawImage(randomCloud, clouds[1].x, clouds[1].y, clouds[1].width, clouds[1].height)
+    ctx.drawImage(randomCloudOne, clouds[2].x, clouds[2].y, clouds[2].width, clouds[2].height ) 
+    
     ctx.drawImage(timerImage, 20, 20, 25, 35)
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "35px Helvetica";
@@ -156,51 +155,107 @@ var render = function () {
 	ctx.textBaseline = "top";
     ctx.fillText(timer, 55, 20);
     
-    //Timer
+  
     ctx.drawImage(scoreImage, 900, 15, 74, 42)
     ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "35px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-    ctx.fillText(score, 870, 20);
+    ctx.fillText(score, 855, 20);
     
     
 };
 function fall() {
-    jumpingStatus = false
-    goingDown = true
+    fallingStatus = true
     falling = setInterval(function() {
-        hero.y +=1;
-    },20)
-
+        player.y +=6;
+    }, 10)
 }
 
 // Update game objects
 var update = function (modifier) {
     if(38 in keysDown) {
-        console.log(hero.y)
         if(!jumpingStatus) {
             jumping = setInterval(jump,10)
         }
     }
-    if (37 in keysDown) { // Player holding left
-        if(hero.x > 5) {
-            hero.x -= hero.speed * modifier;
+    if (37 in keysDown) { 
+        if(player.x > 5) {
+            player.x -= player.speed * modifier;
         }
 
 	}
-    if (39 in keysDown) { // Player holding right
-        hero.x += hero.speed * modifier;
-
+    if (39 in keysDown) { 
+        player.x += player.speed * modifier;    
 
     }
+    checkPosition()
+    
+    if(onCloud) {
+        moveWithCloud()
+    }
+    clouds.forEach(function(cloud) {
+        moveCloud(cloud)
+    })
+ 
+
 
 };
 function checkPosition() {
-    if(( startCloud.y - 130 < hero.y && hero.x > startCloud.x + startCloud.width / 1.2)  || (hero.x > cloud.x + cloud.width / 1.2) || ( hero.y < cloud.y && hero.x + hero.width < cloud.x && !jumpingStatus) ) {
-        fall()
-        
+    clouds.forEach(function(cloud) {
+        if((player.x + 15 < cloud.x + cloud.width) && (player.x + player.width - 15  > cloud.x) && (player.y - player.height === cloud.y - cloud.height) ) {
+            goingDown = true
+            jumpingStatus = false
+            
+        }
+    })
+    /*
+    if(( (cloud.y - player.y > 99) && player.x  > cloud.x + cloud.width / 1.2) ||(cloud.y - player.y > 99 ) && ( player.y < cloud.y && player.x + player.width < cloud.x && !jumpingStatus) ) {
+        goingDown = true
+        fall()       
     }
+        if(( (secondCloud.y - player.y === 98) && player.x  > secondCloud.x + secondCloud.width / 1.2) ||(secondCloud.y - player.y === 98) && ( player.y < secondCloud.y && player.x + player.width < secondCloud.x && !jumpingStatus) ) {
+        goingDown = true
+        fall()       
+    }
+
+    if( fallingStatus &&(player.x + 15 < cloud.x + cloud.width) && (player.x + player.width - 15  > cloud.x) && (player.y + player.height + 3  > cloud.y) && (player.y + player.height < cloud.y + cloud.height) )  {
+        goingDown = false;
+        limit= 200
+        jumpingStatus = false
+        fallingStatus = false
+        clearInterval(falling)
+        onCloud = true
+    }
+    if( fallingStatus && (player.x + 15 < cloudOne.x + cloudOne.width) && (player.x + player.width - 15  > cloudOne.x) && (player.y + player.height > cloudOne.y) && (player.y + player.height < cloudOne.y + cloudOne.height) )  {
+        limit = 325
+        goingDown = false
+        jumpingStatus = false
+        fallingStatus = false
+        clearInterval(falling)
+    }
+*/
+
+clouds.forEach(function(cloud) {
+    if(!goingDown && jumpingStatus && player.y < height / 2) {
+        cloud.y+=8
+    }
+    if(cloud.y > height) {
+        cloud.y = cloud.y - height + 150
+    }
+})
+
+
+}
+
+function moveWithCloud() {
+    clouds.forEach(function(cloud) {
+        if(cloud.y - player.y > 100 && cloud.y - player.y < 150) {
+            player.dir = cloud.dir
+            moveCloud(player)      
+        }
+    })
+
 }
 
 var main = function () {
@@ -210,7 +265,6 @@ var main = function () {
 	update(delta / 1000);
 	render();
 	then = now;
-
     requestAnimationFrame(main);
  
 };
@@ -227,9 +281,6 @@ function resize(){
         resize();
     });
   });
-
-moveCloud(cloud)
-moveCloud(secondCloud)
 
 var then = Date.now();
 main();
