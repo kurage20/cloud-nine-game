32a4e2870e3621ca3a96dc4e556eef926079d845
Darko Sabranovic <dsabranovic@yahoo.com>
2017-08-21 16:14:10 +0200

    fixed scoring

--- src/game.js
+++ src/game.js
@@ -12,15 +12,6 @@ var clouds = []
 var falling
 var jumping
 
-var jumpSound = document.createElement("audio")
-jumpSound.src = "sounds/jump.mp3"
-jumpSound.volume = 0.3
-
-var backgroundMusic = document.createElement("audio")
-backgroundMusic.volume = 0.3
-backgroundMusic.src = "sounds/mario.mp3"
-backgroundMusic.play()
-
 class Player {
     constructor(x, y, speed, width, height, dir, floatSpeed) {
         this.x = x
@@ -52,7 +43,7 @@ Player.prototype.onCloud = false
 Player.prototype.jumpingStatus = false
 Player.prototype.goingDown = false
 Player.prototype.fallingStatus = false
-Player.prototype.limit = 450
+Player.prototype.limit = 425
 
 
 Player.prototype.jump = function () {
@@ -61,6 +52,8 @@ Player.prototype.jump = function () {
         this.jumpingStatus = true
         this.y -= 7;
         jumpSound.play()
+        console.log(this.y + this.height)
+        console.log(clouds[0].y)
         
     } else {
         if (!this.fallingStatus) {
@@ -68,23 +61,21 @@ Player.prototype.jump = function () {
             this.y += 7;
         }
 
-        if (this.y === jump_y) {
-            this.goingDown = false
-            this.jumpingStatus = false
-            clearInterval(jumping);
-        }
         clouds.forEach(function (cloud) {
-            if ((this.x + 15 < cloud.x + cloud.width) && (this.x + this.width - 15 > cloud.x) && (this.y + this.height + 10 > cloud.y) && (this.y + this.height - 20 < cloud.y + cloud.height)) {
-                score++
+            if ((this.x + 15 < cloud.x + cloud.width) && (this.x + this.width - 15 > cloud.x) && (this.y + this.height > cloud.y - 20) && (this.y + this.height + 40 < cloud.y + cloud.height)) {
+                if(!cloud.scored) {
+                    score++
+                    cloud.scored = true
+                }
                 this.goingDown = false;
                 this.jumpingStatus = false
                 this.onCloud = true
                 clearInterval(jumping)
                 if (this.y < 550 && this.y > 300) {
-                    this.limit = 325
+                    this.limit = 300
                 }
                 if (this.y < 400 && this.y > 200) {
-                    this.limit = 225
+                    this.limit = 200
                 }
                 if (this.y < 300 && this.y > 0) {
                     this.limit = 100
@@ -97,15 +88,17 @@ Player.prototype.jump = function () {
 }
 
 class Cloud {
-    constructor(x, y, width, height, dir, floatSpeed) {
+    constructor(x, y, width, height, floatSpeed) {
         this.x = x
         this.y = y
         this.width = width
         this.height = height
-        this.dir = dir
         this.floatSpeed = floatSpeed
     }
 }
+
+Cloud.prototype.scored = false
+Cloud.prototype.dir = "right"
 
 var player = new Player(500, 700, 275, 80, 100, "", 2)
 var jump_y = player.y
@@ -114,11 +107,11 @@ var randomX = function () {
     return Math.random() * (width - 235)
 }
 
-var cloudOne = new Cloud(randomX(), 600, 235, 56, "right", 2)
-var cloudTwo = new Cloud(randomX(), 200, 235, 56, "left", 2.5)
-var cloudThree= new Cloud(randomX(), 400, 235, 56, "right", 3)
+var cloudOne = new Cloud(randomX(), 600, 235, 56, 2)
+var cloudTwoImage = new Cloud(randomX(), 200, 235, 56, 2.5)
+var cloudThree= new Cloud(randomX(), 400, 235, 56, 3)
 
-clouds.push(cloudOne, cloudTwo, cloudThree)
+clouds.push(cloudOne, cloudTwoImage, cloudThree)
 
 
 var timerTick = setInterval(function () {
@@ -133,22 +126,22 @@ var bgImage = new Image();
 var playerImage = new Image();
 var timerImage = new Image()
 var scoreImage = new Image()
-var startCloudImage = new Image()
-var randomCloud = new Image()
-var randomCloudOne = new Image()
+var cloudOneImage = new Image()
+var cloudTwoImage = new Image()
+var cloudThreeImage = new Image()
 
-var cloudImages = ["../PNG/cloud1.png", "../PNG/cloud2.png", "../PNG/cloud3.png"]
-var getRandomCloud = function () {
+var cloudImages = ["img/cloud1.png", "img/cloud2.png", "img/cloud3.png"]
+var randomCloudImage = function () {
     return cloudImages[Math.floor(Math.random() * cloudImages.length)]
 }
 
-bgImage.src = "../PNG/background.png";
-playerImage.src = "../PNG/JumpJumpBoy.png";
-timerImage.src = "../PNG/clock.png"
-scoreImage.src = "../PNG/cloud_counter.png"
-startCloudImage.src = getRandomCloud()
-randomCloud.src = getRandomCloud()
-randomCloudOne.src = getRandomCloud()
+bgImage.src = "img/background.png";
+playerImage.src = "img/JumpJumpBoy.png";
+timerImage.src = "img/clock.png"
+scoreImage.src = "img/cloud_counter.png"
+cloudOneImage.src = randomCloudImage()
+cloudTwoImage.src = randomCloudImage()
+cloudThreeImage.src = randomCloudImage()
 
 // Handle keyboard controls
 var keysDown = {};
@@ -165,9 +158,15 @@ addEventListener("keyup", function (e) {
 var render = function () {
     ctx.drawImage(bgImage, 0, 0);
     ctx.drawImage(playerImage, player.x, player.y, player.width, player.height);
-    ctx.drawImage(startCloudImage, clouds[0].x, clouds[0].y, clouds[0].width, clouds[0].height)
-    ctx.drawImage(randomCloud, clouds[1].x, clouds[1].y, clouds[1].width, clouds[1].height)
-    ctx.drawImage(randomCloudOne, clouds[2].x, clouds[2].y, clouds[2].width, clouds[2].height)
+    ctx.drawImage(cloudOneImage, clouds[0].x, clouds[0].y, clouds[0].width, clouds[0].height)
+    ctx.drawImage(cloudTwoImage, clouds[1].x, clouds[1].y, clouds[1].width, clouds[1].height)
+    ctx.drawImage(cloudThreeImage, clouds[2].x, clouds[2].y, clouds[2].width, clouds[2].height)
+
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(0, 52, 135, 0.4)"
+    ctx.rect(0, 3, width, 70);
+    ctx.fill();
+    ctx.closePath();
 
     ctx.drawImage(timerImage, 20, 20, 25, 35)
     ctx.fillStyle = "rgb(250, 250, 250)";
@@ -184,7 +183,7 @@ var render = function () {
     ctx.textBaseline = "top";
     ctx.fillText(score, 855, 20);
 
-
+ 
 };
 
 // Update game objects
@@ -214,7 +213,6 @@ var update = function (modifier) {
     clouds.forEach(function (cloud) {
         move(cloud)
     })
-    console.log(player.y)
 
 };
 
@@ -223,18 +221,22 @@ function checkPosition() {
         if ((player.x + 15 < cloud.x + cloud.width) && (player.x + player.width - 15 > cloud.x) && (player.y - player.height === cloud.y - cloud.height)) {
             goingDown = true
             jumpingStatus = true
-
         }
         if (!player.goingDown && player.jumpingStatus && player.y < height / 2) {
-            cloud.y += 12
+            cloud.y += 16
+        } else if (!player.goingDown && player.jumpingStatus && player.y < height / 3) {
+            cloud.y += 20
         }
+
         if (cloud.y > height) {
             cloud.y = cloud.y - height + 200
+            cloud.x = Math.floor(Math.random() * 900) + -100
             cloud.floatSpeed += 0.5
+            cloud.scored = false
         }
         if (player.goingDown && player.jumpingStatus && player.y < height / 2) {
             if (player.onCloud) {
-                cloud.y -= 3
+                cloud.y -= 14
             }
 
         }
@@ -299,6 +301,15 @@ $(document).ready(function () {
         resize();
     });
 });
+
+var jumpSound = document.createElement("audio")
+jumpSound.src = "sound/jump.mp3"
+jumpSound.volume = 0.3
+
+var backgroundMusic = document.createElement("audio")
+backgroundMusic.volume = 0.3
+backgroundMusic.src = "sound/mario.mp3"
+backgroundMusic.play()
 
 var then = Date.now();
 main();
